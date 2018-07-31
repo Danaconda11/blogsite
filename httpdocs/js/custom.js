12989a435ef99048640157258685b08556c17533
Daniel <dbaldesarra9@gmail.com>
2018-07-30 22:15:59 -0400

    backup

---
+++ httpdocs/js/custom.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+	//FOUC solution
+	setTimeout(() => {
+		document.getElementsByTagName('body')[0].style.visibility = 'visible'
+	}, 50);
+
+	///highlight nav bar based on location
+	var url = location.href.split("/");
+	var navLinks = document.getElementsByTagName("nav")[0].getElementsByTagName("a");
+	var i = 0;
+	var currentPage = url[url.length - 1];
+	for (i; i < navLinks.length; i++) {
+		var lb = navLinks[i].href.split("/");
+		if (lb[lb.length - 1] == currentPage) {
+			navLinks[i].id = "current";
+		}
+	}
+
+	//update copywrite date
+	document.getElementById('year').innerHTML = new Date().getFullYear()
+}, false);
+
+
+
